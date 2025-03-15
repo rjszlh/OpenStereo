2a13bebd8aa859a3dbb1fb11b2c39178b459feae
rjszlh <1258850987@qq.com>
2025-03-15 13:53:26 +0800

    训练数据集地址更改

--- cfgs/data_basic.py
+++ cfgs/data_basic.py
@@ -1,11 +1,11 @@
 # @Time    : 2024/4/1 13:41
 # @Author  : zhangchenming
 DATA_PATH_DICT = {
-    'SceneFlowDataset': '/mnt/nas/algorithm/chenming.zhang/dataset/SceneFlow',
-    'FlyingThings3DSubsetDataset': '/mnt/nas/algorithm/chenming.zhang/dataset/SceneFlow',
-    'KittiDataset12': '/mnt/nas/algorithm/chenming.zhang/dataset/KITTI/stereo_2012',
-    'KittiDataset15': '/mnt/nas/algorithm/chenming.zhang/dataset/KITTI/stereo_2015',
-    'DrivingDataset': '/mnt/nas/algorithm/chenming.zhang/dataset/DrivingStereo',
-    'MiddleburyDataset': '/mnt/nas/algorithm/chenming.zhang/dataset/Middlebury',
-    'ETH3DDataset': '/mnt/nas/algorithm/chenming.zhang/dataset/ETH3D'
+    'SceneFlowDataset': '/home/hello/dataset/SceneFlow',
+    'FlyingThings3DSubsetDataset': '/home/hello/dataset/SceneFlow',
+    'KittiDataset12': '/home/hello/datasets/kitti12',
+    'KittiDataset15': '/home/hello/datasets/kitti15',
+    'DrivingDataset': '/home/hello/dataset/DrivingStereo',
+    'MiddleburyDataset': '/home/hello/dataset/Middlebury',
+    'ETH3DDataset': '/home/hello/dataset/ETH3D'
 }
